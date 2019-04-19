cafae7571bb58ab8e20da3d7c9d7350617a47c85
Raquel Romo <rqlromo@gmail.com>
2019-04-19 17:46:03 +0200

    dinamic html

--- app.js
+++ app.js
@@ -9,18 +9,34 @@ var app = express();
 app.use(express.static(__dirname + '/'));
 
 
-// o nos puede devolver un fichero con sendFile() , __dirname es un objeto global de Node que te da la ruta a tu directorio raíz actual. es útil cuando queremos evitar escribir rutas dinámicas o largas.
-app.get("/html", function (req, res) {
-  res.sendFile(__dirname + '/sendFile.html');
+// Por defecto el boilerplate de Express utiliza Jade, nosotros queremos utilizar ejs en su lugar por lo que tendremos que setear view engine, el motor de plantilla que se utiliza ejs, los archivos de ejs tendran que estar en la carpeta views
+app.set('view engine', 'ejs');
+
+
+// utilizamos render para visualizar el fichero ejs que deseemos
+app.get("/htmlDinamic", function (req, res) {
+  res.render('htmlDinamic', {name: 'Colega'})
 });
 
-//al hacer un post nos puede responder un testo o nos puede devolver un html tambien
-app.post("/resPost", function (req, res) {
-  // res.send("Soy la respuesta de un form");
-  res.sendFile(__dirname + '/resPost.html');
-});
 
 // Usamos el método app.listen para escuchar por el puerto. Este método necesita dos argumentos: un puerto y una función callback (de respuesta) que indica qué hacer una vez que el servidor esté funcionando.
 app.listen(3000, function () {
   console.log('Server is listening on port 3000!!');
 });
+
+
+
+
+
+
+// // use res.render to load up an ejs view file
+
+// // index page 
+// app.get("/", function (req, res) {
+//   res.render('main')
+// });
+
+// // otro page 
+// app.get('/otro', function(req, res) {
+//   res.render('otro', {name: 'raquel'});
+// });
