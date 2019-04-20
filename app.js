56217f1c779c0f95ead58380b43960085551dfc0
Raquel Romo <rqlromo@gmail.com>
2019-04-20 22:05:55 +0200

    refactorized code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { log } from "util";
+
 //necesitamos incluir la dependencia de express para poder usar sus métodos. En NodeJs, cuando quieres acceder a las funcionalidades de una librería o módulo de otro archivo, tienes que requerirlo.
 var express = require('express');
 
@@ -25,45 +27,96 @@ app.get("/", function (req, res) {
 
 app.post("/userData", function (req, res) {
 
+  const renderPersonHasNoInstructions = () => {
+    res.render('instructions',{
+      response: 'El usuario no existe', 
+      direction: '',
+    })
+  };
+
+  const renderCongratsMsg = () => {
+    res.render('instructions',{
+      response: 'Enhorabuena! Has llegado a casa sano y salvo!', 
+      direction: '',
+    })
+  };
+
+  // if(givenUserHasData) {
+  //   console.log('recoveredDirections.dones[req.body.name]',recoveredDirections.dones[req.body.name])
+
+  //   response = req.body.name;
+  //   direction = recoveredDirections.dones[req.body.name];
+
+  //   recoveredDirections.dones[req.body.name].push('vete a la playa');
+
+  //   console.log('recoveredDirections',recoveredDirections)
+  // } else {
+  //   response = req.body.name;
+  //   direction = 'Debes ' + users.directions[req.body.name][0];
+
+  //   const stepsDone = {
+  //     dones:{},
+  //   };
+  //   var array = [];
+  //   array.push(users.directions[req.body.name][0]);
+  //   stepsDone.dones[req.body.name] = array;
+
+  //   fs.writeFile(__dirname + '/data/continue-steps.json', JSON.stringify(stepsDone), function (error) {     console.log(error)
+  //   });
+  // }
+
+  const recoverCurrentInstruction = personData => {
+    return personData.instructions[personData.currentInstruction];
+  };
+
+  const updateCurrentInstruction = personData => {
+    personData.currentInstruction += 1;
+
+    return personData;
+  }
+
+  // const recoveredDirections = JSON.parse(file);
+  // const givenUserHasData = recoveredDirections.dones[req.body.name] === undefined;
+
+  const renderInstruction = (currentInstruction, personName) => {
+    res.render('instructions', {
+      response: 'Yo te ayudare a llegar a casa ' + personName, 
+      direction: currentInstruction,
+    });
+  }
+
+  const giveInstruction = (personData, personName) => {
+    const currentInstruction = recoverCurrentInstruction(personData);
+    renderInstruction(currentInstruction, personName);
+  }
+
+  const updatePeopleData = (peopleData, personData, personName) => {
+    peopleData[personName] = personData;
+    
+    return peopleData;
+  }
+
+  // TODO: Estamos asumiendo que toda persona que existe en la bbdd tiene instrucciones
   fs.readFile(__dirname + '/data/users-and-directions.json', function (error, file) {  
-    var users = JSON.parse(file);
+    let peopleData = JSON.parse(file);
+    let personData = peopleData[req.body.name];
+    const personHasInstructions = personData !== undefined;
+    const personHasArrivedHome = (personData.instructions.length - 1) === personData.currentInstruction;
 
-    if (users.users.includes(req.body.name)) {
+    console.log('personData',personData);
+    console.log('personHasInstructions',personHasInstructions);
 
-
-      fs.readFile(__dirname + '/data/continue-steps.json', function (error, file) {    
-        const recoveredDirections = JSON.parse(file);
-
-          if(recoveredDirections.dones[req.body.name] === 0) {
-            response = req.body.name;
-            direction = 'Debes ' + users.directions[req.body.name][0];
-      
-            const stepsDone = {
-              dones:{},
-            };
-            var array = [];
-            array.push(users.directions[req.body.name][0]);
-            stepsDone.dones[req.body.name] = array;
-      
-            fs.writeFile(__dirname + '/data/continue-steps.json', JSON.stringify(stepsDone), function (error) {     console.log(error)
-            });
-      
-          } else {
-            response = req.body.name;
-            direction = recoveredDirections.dones[req.body.name];
-      
-          }
-          res.render('instructions',{
-            response: 'Yo te ayudare a llegar a casa ' + response, 
-            direction: direction,
-          })
-      });
-
+    if (personHasInstructions) {
+      if (personHasArrivedHome) {
+        renderCongratsMsg();
+      } else {
+        giveInstruction(personData, req.body.name);
+        personData = updateCurrentInstruction(personData);
+        peopleData = updatePeopleData(peopleData, personData, req.body.name);
+        updatePeopleDataInDB(peopleData);
+      }
     } else {
-      res.render('instructions',{
-        response: 'El usuario no existe', 
-        direction: '',
-      })
+      renderPersonHasNoInstructions();
     }
   });
 
